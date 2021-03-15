f7abd403032afcb213cdd7b1711fd646de8369c4
metaliccode <isan.mh69@gmail.com>
2021-03-15 08:39:41 +0700

    tambah logout

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'DashboardController@index')->name('dashboard');
-
 Auth::routes();
+
+// untuk logout
+Route::get('logout', function () {
+    Auth::logout();
+    return Redirect::to('/login');
+});
+
+Route::get('/', 'DashboardController@index')->name('dashboard');
 
 Route::resource('products', 'ProductController');
 
